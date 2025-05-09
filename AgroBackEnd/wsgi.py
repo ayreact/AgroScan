8a8dd8f6ece2e1dc8f2a38c2eda9c0edc98176e2
ayreact <abiolaayokunnu101@gmail.com>
2025-05-09 05:45:39 +0100

    Resolved issue with static files

--- AgroBackEnd/wsgi.py
+++ AgroBackEnd/wsgi.py
@@ -1,9 +1,16 @@
+"""
+WSGI config for AgroBackEnd project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AgroBackEnd.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root='/app/staticfiles') # Adjust root if needed
